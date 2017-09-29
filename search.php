ea30aa5cadd93fc7aefd83d6a5944b3a99c07e92
Drmrsan <drmrsan@gmail.com>
2017-09-30 00:24:17 +0200

    Search feature by post_tags

--- search.php
+++ search.php
@@ -12,10 +12,30 @@
             <!-- Blog Entries Column -->
             <div class="col-md-8">
 
-                <?php $query = "SELECT * FROM posts"; ?>
-                <?php $posts = mysqli_query($connection, $query); ?>
 
-                <?php while ($row = mysqli_fetch_assoc($posts)) : ?>
+                <?php 
+                    if (isset($_POST['submit'])) {
+                        $search = $_POST['search'];
+
+                        $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%'";
+                        $search_query = mysqli_query($connection, $query);
+
+                        if (!$search_query) {
+                            die("Query failed" . mysqli_error($connection));
+                        }
+
+
+                        $count = mysqli_num_rows($search_query);
+
+
+                        if ($count == 0) {
+                            echo "No result";
+                        }
+                    }
+
+                ?>
+
+                <?php while ($row = mysqli_fetch_assoc($search_query)) : ?>
                    <?php 
                         $post_title = $row['post_title'];
                         $post_author = $row['post_author'];
